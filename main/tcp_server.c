d40be4e0f9a39c11641098f9cde47a629d7f9a2d
Cyfarwydd <directorluo@qq.com>
2024-05-07 16:30:54 +0800

    new empty task st ver

--- main/tcp_server.c
+++ main/tcp_server.c
@@ -10,6 +10,7 @@
 #include <sys/param.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
+#include "freertos/queue.h"
 #include "esp_system.h"
 #include "esp_wifi.h"
 #include "esp_event.h"
@@ -23,35 +24,50 @@
 #include "lwip/sys.h"
 #include <lwip/netdb.h>
 
-
-#define PORT                        CONFIG_EXAMPLE_PORT
-#define KEEPALIVE_IDLE              CONFIG_EXAMPLE_KEEPALIVE_IDLE
-#define KEEPALIVE_INTERVAL          CONFIG_EXAMPLE_KEEPALIVE_INTERVAL
-#define KEEPALIVE_COUNT             CONFIG_EXAMPLE_KEEPALIVE_COUNT
+#define PORT CONFIG_EXAMPLE_PORT
+#define KEEPALIVE_IDLE CONFIG_EXAMPLE_KEEPALIVE_IDLE
+#define KEEPALIVE_INTERVAL CONFIG_EXAMPLE_KEEPALIVE_INTERVAL
+#define KEEPALIVE_COUNT CONFIG_EXAMPLE_KEEPALIVE_COUNT
 
 static const char *TAG = "example";
+
+typedef struct
+{
+    float az; // Azimuth
+    float el; // Elevation
+} Antena_rot;
+
+Antena_rot rot_data;
 
 static void do_retransmit(const int sock)
 {
     int len;
     char rx_buffer[128];
 
-    do {
+    do
+    {
         len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-        if (len < 0) {
+        if (len < 0)
+        {
             ESP_LOGE(TAG, "Error occurred during receiving: errno %d", errno);
-        } else if (len == 0) {
+        }
+        else if (len == 0)
+        {
             ESP_LOGW(TAG, "Connection closed");
-        } else {
+        }
+        else
+        {
             rx_buffer[len] = 0; // Null-terminate whatever is received and treat it like a string
             ESP_LOGI(TAG, "Received %d bytes: %s", len, rx_buffer);
 
             // send() can return less bytes than supplied length.
             // Walk-around for robust implementation.
             int to_write = len;
-            while (to_write > 0) {
+            while (to_write > 0)
+            {
                 int written = send(sock, rx_buffer + (len - to_write), to_write, 0);
-                if (written < 0) {
+                if (written < 0)
+                {
                     ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
                     // Failed to retransmit, giving up
                     return;
@@ -65,7 +81,7 @@ static void do_retransmit(const int sock)
 static void tcp_server_task(void *pvParameters)
 {
     char addr_str[128];
-    int addr_family = (int)pvParameters;
+    int addr_family = (int)AF_INET;
     int ip_protocol = 0;
     int keepAlive = 1;
     int keepIdle = KEEPALIVE_IDLE;
@@ -74,7 +90,8 @@ static void tcp_server_task(void *pvParameters)
     struct sockaddr_storage dest_addr;
 
 #ifdef CONFIG_EXAMPLE_IPV4
-    if (addr_family == AF_INET) {
+    if (addr_family == AF_INET)
+    {
         struct sockaddr_in *dest_addr_ip4 = (struct sockaddr_in *)&dest_addr;
         dest_addr_ip4->sin_addr.s_addr = htonl(INADDR_ANY);
         dest_addr_ip4->sin_family = AF_INET;
@@ -83,7 +100,8 @@ static void tcp_server_task(void *pvParameters)
     }
 #endif
 #ifdef CONFIG_EXAMPLE_IPV6
-    if (addr_family == AF_INET6) {
+    if (addr_family == AF_INET6)
+    {
         struct sockaddr_in6 *dest_addr_ip6 = (struct sockaddr_in6 *)&dest_addr;
         bzero(&dest_addr_ip6->sin6_addr.un, sizeof(dest_addr_ip6->sin6_addr.un));
         dest_addr_ip6->sin6_family = AF_INET6;
@@ -93,7 +111,8 @@ static void tcp_server_task(void *pvParameters)
 #endif
 
     int listen_sock = socket(addr_family, SOCK_STREAM, ip_protocol);
-    if (listen_sock < 0) {
+    if (listen_sock < 0)
+    {
         ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
         vTaskDelete(NULL);
         return;
@@ -109,7 +128,8 @@ static void tcp_server_task(void *pvParameters)
     ESP_LOGI(TAG, "Socket created");
 
     int err = bind(listen_sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
-    if (err != 0) {
+    if (err != 0)
+    {
         ESP_LOGE(TAG, "Socket unable to bind: errno %d", errno);
         ESP_LOGE(TAG, "IPPROTO: %d", addr_family);
         goto CLEAN_UP;
@@ -117,19 +137,22 @@ static void tcp_server_task(void *pvParameters)
     ESP_LOGI(TAG, "Socket bound, port %d", PORT);
 
     err = listen(listen_sock, 1);
-    if (err != 0) {
+    if (err != 0)
+    {
         ESP_LOGE(TAG, "Error occurred during listen: errno %d", errno);
         goto CLEAN_UP;
     }
 
-    while (1) {
+    while (1)
+    {
 
         ESP_LOGI(TAG, "Socket listening");
 
         struct sockaddr_storage source_addr; // Large enough for both IPv4 or IPv6
         socklen_t addr_len = sizeof(source_addr);
         int sock = accept(listen_sock, (struct sockaddr *)&source_addr, &addr_len);
-        if (sock < 0) {
+        if (sock < 0)
+        {
             ESP_LOGE(TAG, "Unable to accept connection: errno %d", errno);
             break;
         }
@@ -141,12 +164,14 @@ static void tcp_server_task(void *pvParameters)
         setsockopt(sock, IPPROTO_TCP, TCP_KEEPCNT, &keepCount, sizeof(int));
         // Convert ip address to string
 #ifdef CONFIG_EXAMPLE_IPV4
-        if (source_addr.ss_family == PF_INET) {
+        if (source_addr.ss_family == PF_INET)
+        {
             inet_ntoa_r(((struct sockaddr_in *)&source_addr)->sin_addr, addr_str, sizeof(addr_str) - 1);
         }
 #endif
 #ifdef CONFIG_EXAMPLE_IPV6
-        if (source_addr.ss_family == PF_INET6) {
+        if (source_addr.ss_family == PF_INET6)
+        {
             inet6_ntoa_r(((struct sockaddr_in6 *)&source_addr)->sin6_addr, addr_str, sizeof(addr_str) - 1);
         }
 #endif
@@ -163,22 +188,36 @@ CLEAN_UP:
     vTaskDelete(NULL);
 }
 
+static void rotator_controller(void *pvParameters)
+{
+    while (1)
+    {
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(nvs_flash_init());
     ESP_ERROR_CHECK(esp_netif_init());
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-    /* This helper function configures Wi-Fi or Ethernet, as selected in menuconfig.
-     * Read "Establishing Wi-Fi or Ethernet Connection" section in
-     * examples/protocols/README.md for more information about this function.
-     */
     ESP_ERROR_CHECK(example_connect());
 
+    // need two task, one part take charge recviving data from Look4sat, the other in charge rotator controlling
+
+    QueueHandle_t RotQueueHandler;                          // create rotator queue handler
+    RotQueueHandler = xQueueCreate(5, sizeof(Antena_rot));  // create message queue
+    
+    if (RotQueueHandler != NULL)
+    {
+        xTaskCreate(rotator_controller, "rotator_controller", 1024, (void *)RotQueueHandler, 3, NULL);
+        
 #ifdef CONFIG_EXAMPLE_IPV4
-    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET, 5, NULL);
+    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void *)RotQueueHandler, 5, NULL);
 #endif
 #ifdef CONFIG_EXAMPLE_IPV6
-    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void*)AF_INET6, 5, NULL);
+    xTaskCreate(tcp_server_task, "tcp_server", 4096, (void *)AF_INET6, 5, NULL);
 #endif
+    }
 }
