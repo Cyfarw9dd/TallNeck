f56dda0b30ba68a6370f65327658222ce9c090ab
Cyfarwydd <Cyfarw9dd@outlook.com>
2024-09-27 16:57:19 +0800

    feat: 添加http事件处理函数，当前文件下载内容出现问题

--- main/src/main.c
+++ main/src/main.c
@@ -48,8 +48,8 @@
 #define CONN_PERIOD             pdMS_TO_TICKS(10000)
 #define RECV_PERIOD             pdMS_TO_TICKS(50)
 #define FILE_URL                "https://celestrak.org/NORAD/elements/gp.php?CATNR=25544&FORMAT=tle"
-#define FILE_PATH               "/littlefs/iss.txt"
-#define WIFI_CONNECTED_BIT BIT0
+#define FILE_PATH               "/littlefs/ISS(ZARYA).txt"
+#define WIFI_CONNECTED_BIT      BIT0
 
 char test_buffer[128];
 
@@ -83,19 +83,74 @@ static void led_timer_callback(TimerHandle_t xTimer)
     }
 }
 
+esp_err_t _http_event_handler(esp_http_client_event_t *evt)
+{
+    static char *output_buffer;  // 用于存储HTTP请求响应的缓冲区
+    static int output_len;       // 存储已读取的字节数
+    switch(evt->event_id) {
+        case HTTP_EVENT_ON_DATA:
+            if (!esp_http_client_is_chunked_response(evt->client)) {
+                // 如果配置了user_data缓冲区，将响应复制到缓冲区
+                if (evt->user_data) {
+                    memcpy(evt->user_data + output_len, evt->data, evt->data_len);
+                } else {
+                    if (output_buffer == NULL) {
+                        output_buffer = (char *) malloc(esp_http_client_get_content_length(evt->client));
+                        output_len = 0;
+                        if (output_buffer == NULL) {
+                            ESP_LOGE(TAG, "Failed to allocate memory for output buffer");
+                            return ESP_FAIL;
+                        }
+                    }
+                    memcpy(output_buffer + output_len, evt->data, evt->data_len);
+                }
+                output_len += evt->data_len;
+            }
+            break;
+        case HTTP_EVENT_ON_FINISH:
+            if (output_buffer != NULL) {
+                // 在这里处理完整的响应数据，例如写入文件
+                FILE* f = fopen(FILE_PATH, "w");
+                if (f == NULL) {
+                    ESP_LOGE(TAG, "Failed to open file for writing");
+                } else {
+                    ESP_LOGI(TAG, "Writing data to the file.\n");
+                    fprintf(f, "%s", output_buffer);
+                    fclose(f);
+                }
+                free(output_buffer);
+                output_buffer = NULL;
+            }
+            output_len = 0;
+            break;
+        default:
+            break;
+    }
+    return ESP_OK;
+}
+
 void download_tle_task(void *pvParameters)
 {
     ESP_LOGI(TAG, "Downloading TLE data from URL");
 
     esp_http_client_config_t config = {
-        .url = FILE_URL,
+        .url = FILE_URL,  // 页面URL
+        /**
+         * @brief   如果是普通的HTTP连接，那么是不需要配置证书的，而HTTPS需要
+         */
         .cert_pem = NULL,  // 用于配置指定服务器的SSL证书，NULL代表使用默认证书
-        .skip_cert_common_name_check = true,
-        .transport_type = HTTP_TRANSPORT_OVER_SSL,
-        .crt_bundle_attach = &esp_crt_bundle_attach,
+        .skip_cert_common_name_check = true,  // 跳过证书通用名称的检查
+        .transport_type = HTTP_TRANSPORT_OVER_SSL,  // 确保客户端和服务器端的通信是加密的，指定使用SSL/TLS加密的HTTP连接
+        /**
+         * @brief   esp_crt_bundle_attach 是 ESP-IDF 提供的一个函数，它会加载一个包含多个受信任 CA 证书的捆绑包。
+         *          这样可以验证各种不同的服务器证书，而不需要单独指定每个证书。
+         */
+        .crt_bundle_attach = esp_crt_bundle_attach,  
+        .event_handler = _http_event_handler,  // 定义事件处理函数
     };
+    
     esp_http_client_handle_t client = esp_http_client_init(&config);
-
+    esp_http_client_set_method(client, HTTP_METHOD_GET);
     esp_err_t err = esp_http_client_perform(client);
     if (err == ESP_OK) {
         ESP_LOGI(TAG, "HTTP GET Status = %d, content_length = %lld",
@@ -112,13 +167,44 @@ void download_tle_task(void *pvParameters)
 
         char buffer[128];
         int data_read;
-        while ((data_read = esp_http_client_read(client, buffer, sizeof(buffer))) > 0) {
-            fwrite(buffer, 1, data_read, f);
-        }
+        // NOTE: data_read返回0，导致没有任何内容被写入至文件中
+        do
+        {
+            data_read = esp_http_client_read(client, buffer, sizeof(buffer));
+            if (fwrite(buffer, 1, data_read, f) != data_read) 
+            {
+                ESP_LOGE(TAG, "Failed to write data to file");
+                fclose(f);
+                esp_http_client_cleanup(client);
+                vTaskDelete(NULL);
+                return;
+            }
+            // 打印读取的数据
+            printf("%.*s", data_read, buffer);
+
+            if (data_read < 0 || 0 == data_read) 
+            {
+                ESP_LOGE(TAG, "Error occurred during reading: %d", data_read);
+            }
+        } while(data_read > 0);
 
         fclose(f);
+
+        // 在这里对下载的数据进行检验
+        FILE *fp = fopen(FILE_PATH, "r");
+        ESP_LOGI(TAG, "Showing the content of the file: \n");
+        char temp_buffer[128];
+
+        while (fgets(temp_buffer, sizeof(temp_buffer), fp))
+        {
+            ESP_LOGW(TAG, "%s", temp_buffer);
+        }
+        fclose(fp);
+
         ESP_LOGI(TAG, "File written successfully");
-    } else {
+    } 
+    else 
+    {
         ESP_LOGE(TAG, "HTTP GET request failed: %s", esp_err_to_name(err));
     }
 
@@ -139,7 +225,7 @@ void cb_connection_ok(void *pvParameter)
 
 	ESP_LOGI(TAG, "I have a connection and my IP is %s!", str_ip);
     // 在这里触发数据下载任务
-    xTaskCreate(download_tle_task, "download_tle", 4096, NULL, 5, NULL);
+    xTaskCreate(download_tle_task, "download_tle", 8192, NULL, 5, NULL);
 
 }
 
@@ -147,7 +233,7 @@ void app_main(void)
 {
     Led_Init();  // LED初始化
     littlefs_init(&littlefs_conf);  // LittleFS 文件系统初始化
-    // esp_err_t err = nvs_flash_erase();
+    // esp_err_t err = nvs_flash_erase();  // 用于擦除nvs部分
     LedTimerHandle = xTimerCreate("led_controller", NOTCONN_PERIOD, pdTRUE, 0, led_timer_callback);  // 创建LED定时器
     RotQueueHandler = xQueueCreate(5, sizeof(Tcp_Sentence *));  // 创建用于传输俯仰角数据的消息队列
 
@@ -161,14 +247,12 @@ void app_main(void)
     // 回调函数，用于返回IP 
 	wifi_manager_set_callback(WM_EVENT_STA_GOT_IP, &cb_connection_ok);
 
-    ESP_LOGE(TAG, "hello");
-    if (RotQueueHandler != NULL)
-    {
-        xTaskCreate(rotator_controller, "rotator_control", 4096, (void *)RotQueueHandler, 3, NULL);
-        xTaskCreate(tcp_server_task, "tcp_server", 4096, (void *)RotQueueHandler, 5, NULL);
+    // if (RotQueueHandler != NULL)
+    // {
+    //     xTaskCreate(rotator_controller, "rotator_control", 4096, (void *)RotQueueHandler, 3, NULL);
+    //     xTaskCreate(tcp_server_task, "tcp_server", 4096, (void *)RotQueueHandler, 5, NULL);
 
-        LedStatus = NOTCONNECTED;
-    }
-    
+    //     LedStatus = NOTCONNECTED;
+    // }
 
 }
